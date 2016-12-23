fd7f2a9351d23f00802a18b9760880c44d217912
zishan sami <zishansami102@gmail.com>
2016-12-23 21:53:33 +0530

    fix: change partition function to cover all the cases

--- quick_sort.py
+++ quick_sort.py
@@ -11,57 +11,35 @@ def swap(array, j, k):
 
 # partition definition
 def partition(array, pivot):
-	j = 0
-	k = len(array)-1
-	while(1):
-		while(array[pivot] > array[j]):
-			j = j + 1
-			if j == len(array) - 1:
-				break
-		while(array[pivot] < array[k]):	
-			k = k - 1
-			if k == 0:
-				break
-		if j >= k:
-			break
-		array = swap(array, j, k)
-
-	array = swap(array, pivot, k)
-	return array, k
-
-
+	array = swap(array, 0, pivot)
+	pivot = 0
+	i = 1
+	for j in range(0, len(array)):
+		if array[j] < array[pivot]:
+			array = swap(array, j, i)
+			i = i + 1
+	array = swap(array, pivot, i-1)
+	return array, i-1
 
 # quicksort definition starts here
 def quick_sort(array):
 	n = len(array)
-	if n < 3:
-		if n == 2 and array[0] > array[1]:
-			array = swap(array, 0, 1)
+	if n <= 1:
 		return array
-
-	# pivot = random.randint(0, n-1)
-	# print(pivot)
-	pivot = 0
+	pivot = random.randint(0, n-1)
 	array, pivot = partition(array, pivot)
-	
-	# asplitting array into two parts starts here
+	# splitting array into two parts starts here
 	split1 = list()
-	for i in range(0, pivot):
-		split1.append(array[i])
-
 	split2 = list()
-	for i in range(pivot+1, n):
-		split2.append(array[i])
-
-	# sort the splitted parts recursively
-	sorted_split1 = quick_sort(split1)
-	sorted_split2 = quick_sort(split2)
-	sorted_array = sorted_split1 + [array[pivot]] + sorted_split2
-
-	return sorted_array
+	for i in range(0, n):
+		if i<pivot:
+			split1.append(array[i])
+		if i > pivot:
+			split2.append(array[i])
+	# sort the splitted parts recursively and return
+	return quick_sort(split1) + [array[pivot]] + quick_sort(split2)
 
 #an example set
-array = [2,5,4,3,6,0,1,9,8,7]
-
+array = [24, 12,16,9,5,4,23,18,14,1,10,17,2,13,3,11,21,6,25,7,22,15,19,20,8]
 array = quick_sort(array)
 print(array)
